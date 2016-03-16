be1989ebc10bc61c8df8d63b88422c09b787052e
clux <analsandblaster@gmail.com>
2016-03-16 10:49:25 +0000

    check that output works

--- test/curlcrate/src/main.rs
+++ test/curlcrate/src/main.rs
@@ -4,8 +4,11 @@ use std::process;
 use curl::http;
 
 fn main() {
-    println!("Hello!");
-    let url = "https://github.com/clux/muslrust/blob/master/README.md";
+    let url = "https://raw.githubusercontent.com/clux/muslrust/master/test/curlcrate/src/main.rs";
+
     let resp = http::handle().get(url).exec().unwrap();
+    let body = String::from_utf8_lossy(resp.get_body());
+    println!("{}", body);
     process::exit(if resp.get_code() == 200 { 0 } else { 1 });
+    // NB: This is a quine
 }
