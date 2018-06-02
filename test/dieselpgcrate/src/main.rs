65842bc76786d9d8692e7e943f31c36f0dbd650e
clux <sszynrae@gmail.com>
2018-06-02 15:19:45 +0100

    diesel works without forks, but only up to 1.2 on nightly
    remove reliance on forks and pin version to 1.2 until
    https://github.com/diesel-rs/diesel/issues/1700 gets resolved either in
    rust or diesel.

--- test/dieselpgcrate/src/main.rs
+++ test/dieselpgcrate/src/main.rs
@@ -35,7 +35,7 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
 fn main() {
-    let database_url = std::env::var("DATABASE_URL").unwrap_or("postgres://localhost".into());
+    let database_url = std::env::var("DATABASE_URL").unwrap_or("postgres://localhost?connect_timeout=1".into());
     match PgConnection::establish(&database_url) {
       Err(e) => {
         println!("Should fail to connect here:");
